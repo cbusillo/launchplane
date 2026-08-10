60e639ab81c56c928f168effc4eac4a03ae2b865
Chris Busillo <info@shinycomputers.com>
2026-08-10 17:55:54 -0400

    fix(storage): advance expected schema head

--- control_plane/storage/schema_invariants.py
+++ control_plane/storage/schema_invariants.py
@@ -10,7 +10,7 @@ from sqlalchemy.engine import Engine
 from sqlalchemy.exc import SQLAlchemyError
 
 AUTHZ_COMPATIBILITY_FLOOR_REVISION = "f3b5d7e9a1c2"
-EXPECTED_ALEMBIC_HEAD_REVISION = "f3a5c7e9b1d4"
+EXPECTED_ALEMBIC_HEAD_REVISION = "a4c6e8f0b2d5"
 RUNTIME_COMPATIBLE_ALEMBIC_REVISIONS = (EXPECTED_ALEMBIC_HEAD_REVISION,)
 _AUTHZ_POLICY_TABLE = "launchplane_authz_policies"
 _AUTHZ_POLICY_WRITE_FENCE_TRIGGER = "launchplane_authz_policy_write_fence"
